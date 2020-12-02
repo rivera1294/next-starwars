84308bf79fc20c8c8264d72fc115a85c0c1fb8cf
Jason Rivera <jrivera2850@gmail.com>
2020-12-02 09:25:31 -0600

    Adjust padding and margins

--- components/Nav.js
+++ components/Nav.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function Nav() {
     return (
-        <div className="py-6 px-8 bg-gray-700 text-white flex justify-between">
+        <div className="py-4 px-8 bg-gray-700 text-white flex justify-between">
             <div className="text-xl"><Link href='/'>Starwars API</Link></div>
             <div className="flex">
                 <div className="ml-8"><Link href='/people'>People</Link></div>
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      <h1 className="text-2xl text-center mt-6 font-semibold">Welcome to Next.js Stars API</h1>
+      <h1 className="text-2xl text-center mt-6 font-semibold my-6">Welcome to Next.js Stars API</h1>
       <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat leo sit amet quam dictum vulputate. Duis quis erat molestie ligula vehicula molestie ut vestibulum sapien. Donec sollicitudin neque vitae erat rutrum, vitae condimentum augue imperdiet. Proin dignissim velit a nisi lacinia, a consectetur lacus molestie. Fusce luctus auctor eros sed molestie. Cras purus leo, aliquam eget eros sed, malesuada facilisis mauris. In lacinia congue turpis quis dignissim. Donec lobortis congue felis eget consequat.</p>
 
       <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat leo sit amet quam dictum vulputate. Duis quis erat molestie ligula vehicula molestie ut vestibulum sapien. Donec sollicitudin neque vitae erat rutrum, vitae condimentum augue imperdiet. Proin dignissim velit a nisi lacinia, a consectetur lacus molestie. Fusce luctus auctor eros sed molestie. Cras purus leo, aliquam eget eros sed, malesuada facilisis mauris. In lacinia congue turpis quis dignissim. Donec lobortis congue felis eget consequat.</p>
